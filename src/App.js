64ae4a28467796ebb1e5fc999a06d100a3624887
Luke Loecher <Luke.Loecher@ibm.com>
2022-03-18 15:50:16 -0400

    added routing and an about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedBackList";
 import FeedbackData from "./data/FeedBackData";
 import FeedBackStats from "./components/FeedBackStats";
-import { useState } from "react";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,14 +24,31 @@ function App() {
   };
 
   return (
-    <>
-      <Header text="Feedback UI" />
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedBack} />
-        <FeedBackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
-    </>
+    <Router>
+      <>
+        <Header text="Feedback UI" />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedBack} />
+                  <FeedBackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
+            <Route exact path="/about" emlement={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "../components/shared/Card";
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
